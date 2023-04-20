a077bbd9454fc6b2ec3c9cf12cbcceb088dd9acd
Rylan Dean <84554929+Ninjapenguin16@users.noreply.github.com>
2023-04-19 20:57:04 -0500

    Adding the starting stuff
    Initial tools I've made

---
+++ SourceCode/mv.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+std::string Argf;
+std::string Arg1;
+std::string Arg2;
+
+char Out[255];
+
+int main(int argc, char *argv[]){
+
+if(argc == 3){
+	Arg1 = argv[1];
+	Arg2 = argv[2];
+	Argf = "move " + Arg1 + " " + Arg2;
+
+	for(int i = 0; Argf[i] != NULL; i++){
+
+		Out[i] = Argf[i];	
+
+	}
+
+	system(Out);
+
+}
+else{
+	std::cout << "Wrong Syntax" << std::endl;
+}
+
+}
