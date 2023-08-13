8595604daf99e37405c9ed002012dc90d2d81e5b
Rylan Dean <84554929+Ninjapenguin16@users.noreply.github.com>
2023-08-13 03:05:21 -0500

    Update Setup.bat
    Added colors, future planning, and installer cleanup

--- Executables/Setup.bat
+++ Executables/Setup.bat
@@ -1,15 +1,99 @@
-REM @echo off
+@echo off
 
-winget install Git.Git
+REM Prompt user for what programs they want
+set "FirefoxInstall="
+set "SteamInstall="
 
-echo git installed
+:PromptLoop
 
-winget install cURL.cURL
+REM Building options
+echo [4;33mSelect Programs to install
+echo.
 
-echo curl installed
+if defined FirefoxInstall (
+	echo [0;37m1: Firefox - [0;32mYES
+) else (
+	echo [0;37m1: Firefox - [0;31mNo
+)
 
-pause
+if defined SteamInstall (
+	echo [0;37m2: Steam - [0;32mYES
+) else (
+	echo [0;37m2: Steam - [0;31mNo
+)
 
-curl -L -o Firefox.exe "https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US"
+echo [0;37m98: Select All
 
-start Firefox.exe
+echo [0;37m99: Confirm Choices
+
+echo. [0;37m
+set /p "Choice=Select Option: "
+set /A Choice=%Choice%
+
+cls
+
+
+REM Choice logic
+if %Choice% == 0 (
+	echo [0;91mEnter a valid choice
+	goto PromptLoop
+)
+if %Choice% == 1 (
+	if defined FirefoxInstall (
+		set "FirefoxInstall="
+	) else (
+		set "FirefoxInstall=defined"
+	)
+	goto PromptLoop
+)
+if %Choice% == 2 (
+	if defined SteamInstall (
+		set "SteamInstall="
+	) else (
+		set "SteamInstall=defined"
+	)
+	goto PromptLoop
+)
+
+REM Select All option
+if %Choice% == 98 (
+	set "FirefoxInstall=defined"
+	set "SteamInstall=defined"
+)
+if %Choice% == 99 (
+	goto BreakPromptLoop
+)
+
+goto PromptLoop
+
+:BreakPromptLoop
+
+
+
+REM Downloading and Installing
+
+mkdir TempInstallers
+
+if defined FirefoxInstall (
+	echo [4;92mDownloading Firefox[0;33m
+	curl -L -o "TempInstallers\Firefox.exe" "https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US"
+	echo.
+)
+
+if defined SteamInstall (
+	echo [4;92mDownloading Steam[0;33m
+	curl -L -o "TempInstallers\SteamSetup.exe" "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe"
+	echo.
+)
+
+if exist "TempInstallers\Firefox.exe" (
+	start /wait .\TempInstallers\Firefox.exe
+)
+if exist "TempInstallers\SteamSetup.exe" (
+	start /wait .\TempInstallers\SteamSetup.exe
+)
+
+rmdir /s /q TempInstallers
+
+echo Script has finished, enjoy your programs :)
+timeout 5 >nul
